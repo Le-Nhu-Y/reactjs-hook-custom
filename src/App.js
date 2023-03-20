124e2debe69dd357e1bf399465da3d5b09d0e3f1
Le-Nhu-Y <lenhuy20021989@gmail.com>
2023-03-20 16:11:45 +0700

    Add userClock.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MyClock from './components/MyClock.js';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyClock/>
       </header>
     </div>
   );
